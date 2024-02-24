3d7ed3aaa1087f9415db3c0ed1ef5f417c4e44a6
Zack Gottesman <zachary.s.gottesman.22@dartmouth.edu>
2024-02-24 00:25:26 -0500

    finish day 4 part 1

--- day4.py
+++ day4.py
@@ -1,16 +1,19 @@
 def main(input_file):
     # set counter to store amount of winning cards
     # we do this all together because
+    total_pts = 0
     with open(input_file, 'r') as file:
         for line in file:
-            process_line(line)
+            total_pts += process_line(line)
+
+    return total_pts
 
 def process_line(line):
     data = line.split(": ")[1]
     data = data.split(" | ")
     winners = clean_numbers(data[0])
     our_numbers = clean_numbers(data[1])
-    print(winners, "***", our_numbers)
+    return process_card(winners, our_numbers)
 
 def clean_numbers(line):
   nums = line.split()
@@ -19,5 +22,14 @@ def clean_numbers(line):
 
   return nums
   
+def process_card(winners, our_nums):
+  total_winners = 0
+  for winner in winners:
+    if winner in our_nums:
+      total_winners += 1
 
-main("inputs/testinput4.1")
+  return 0 if total_winners == 0 else 2 ** (total_winners - 1)
+
+print(main("inputs/testinput4.1"))
+print(main("inputs/input4"))
+

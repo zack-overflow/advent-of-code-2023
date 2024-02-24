cab0073f55d64f9528ff06d851baa87f92d288ee
Zack Gottesman <zachary.s.gottesman.22@dartmouth.edu>
2024-02-23 23:43:52 -0500

    Adding parsing of numbers for day 4

--- day4.py
+++ day4.py
@@ -1,5 +1,5 @@
 def main(input_file):
-	# set counter to store amount of winning cards
+    # set counter to store amount of winning cards
     # we do this all together because
     with open(input_file, 'r') as file:
         for line in file:
@@ -7,7 +7,17 @@ def main(input_file):
 
 def process_line(line):
     data = line.split(": ")[1]
+    data = data.split(" | ")
     winners = clean_numbers(data[0])
     our_numbers = clean_numbers(data[1])
+    print(winners, "***", our_numbers)
+
+def clean_numbers(line):
+  nums = line.split()
+  for i in range(len(nums)):
+    nums[i] = int(nums[i])
+
+  return nums
+  
 
 main("inputs/testinput4.1")
